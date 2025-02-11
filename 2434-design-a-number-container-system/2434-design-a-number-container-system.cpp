1fae9693d7b1ba18764a90ddd96911edcfd7f1a9
Sebastian-Dante <89245508+Sebastian-Dante@users.noreply.github.com>
2025-02-11 11:09:23 +0100

    Time: 143 ms (21.79%), Space: 195.7 MB (63.85%) - LeetHub

---
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -0,0 +1,40 @@
+class NumberContainers {
+private:
+    unordered_map<int, int> indexToNumber; // Maps index to number
+    unordered_map<int, map<int, bool>> numberToIndices; // Maps number to a sorted map of indices
+public:
+    NumberContainers() {}
+        
+
+        
+    
+    
+    void change(int index, int number) {
+        if (indexToNumber.find(index) != indexToNumber.end()) {
+            int oldNumber = indexToNumber[index];
+            numberToIndices[oldNumber].erase(index);
+            if (numberToIndices[oldNumber].empty()) {
+                numberToIndices.erase(oldNumber);
+            }
+        }
+
+        indexToNumber[index] = number;
+        numberToIndices[number][index] = true;
+    }
+    
+    
+    int find(int number) {
+        if (numberToIndices.find(number) != numberToIndices.end() && !numberToIndices[number].empty()) {
+            return numberToIndices[number].begin() -> first;
+        }
+        return -1;
+        
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
