ce3452544307187fc915a512eae53f4ce0201de4
Sebastian-Dante <89245508+Sebastian-Dante@users.noreply.github.com>
2025-02-13 13:56:03 +0100

    Time: 0 ms (100%), Space: 28.1 MB (84.38%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0, count = 0;
+
+        // Boyer-Moore Voting Algorithm
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num; // Set a new candidate
+            }
+            count += (num == candidate) ? 1 : -1; // Increase or decrease count
+        }
+
+        return candidate; // The majority element is guaranteed to exist
+    }
+};
