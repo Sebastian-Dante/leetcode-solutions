a1c95ced827798a4d7e38e7c66bde8f2a5564a3e
Sebastian-Dante <89245508+Sebastian-Dante@users.noreply.github.com>
2025-02-13 13:54:21 +0100

    Time: 87 ms (11.88%), Space: 20.5 MB (9.44%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # If the array has less than 3 elements, no need to modify it-
+        if len(nums) <= 2:
+            return len(nums)
+
+        # Pointer for the position to place the next allowed element
+        index = 2
+
+        # Iterate over the array starting from the third element.
+        for i in range(2, len(nums)):
+            # If the current element is not equal to the element at index-2,
+            # It means it has appeared at most twice, so we keep it.
+            if nums[i] != nums[index - 2]:
+                nums[index] = nums[i] #Place the element at the correct position.
+                index +=1 # Move the index forward
+        
+        # The value of index represents the new length of the modified array.
+        return index
